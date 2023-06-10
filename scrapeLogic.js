2222eeb29f3f571d014be4e8cee8557dea7b9e40
webapisbr <dev@webapis.com.br>
2023-06-10 00:22:44 -0300

    update

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -8,6 +8,12 @@ const scrapeLogic = async (res) => {
     
     const browser = await puppeteer.launch({
         headless: 'new',
+        args: [
+            "--disable-setupid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",
+        ],
         executablePath:
             process.env.NODE_ENV === "production"
                 ? process.env.PUPPETEER_EXECUTABLE_PATH
