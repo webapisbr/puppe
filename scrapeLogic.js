3396c2847c6bdee58129bf7f2c56e44208a5356f
webapisbr <dev@webapis.com.br>
2023-06-09 22:23:35 -0300

    first commit

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -11,22 +11,22 @@ const scrapeLogic = async (res) => {
         await page.setViewport({width: 1080, height: 1024});
 
         // Type into search box
-        await page.type('.search-box__input', 'automate beyond recorder');
+        const title = await page.title();
 
         // Wait and click on first result
-        const searchResultSelector = '.search-box__link';
+        /*const searchResultSelector = '.search-box__link';
         await page.waitForSelector(searchResultSelector);
-        await page.click(searchResultSelector);
+        await page.click(searchResultSelector);*/
 
         // Locate the full title with a unique string
-        const textSelector = await page.waitForSelector(
+        /*const textSelector = await page.waitForSelector(
             'text/Customize and automate'
         );
-        const fullTitle = await textSelector?.evaluate(el => el.textContent);
+        const fullTitle = await textSelector?.evaluate(el => el.textContent);*/
 
         // Print the full title
-        console.log('The title of this blog post is "%s".', fullTitle);
-        res.send(fullTitle);
+        console.log('The title of this blog post is "%s".', title);
+        res.send(title);
         
     }catch (e){
         console.error(e);
